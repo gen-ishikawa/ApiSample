aa19187e6c06b5014cbded2cfced5d5f8986aba6
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-10-13 06:05:25 +0000

    showアクションの追加

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -3,4 +3,18 @@ class PoemsController < ApplicationController
     @poems = Poem.all
     render json: @poems
   end
+
+  def show
+    set_poem
+    render json: @poems
+  end
+
+  private
+    def set_poem
+      @poems = Poem.find(params[:id])
+    end
+
+    def poem_params
+      params.require(:poem).permit(:title, :content)
+    end
 end
